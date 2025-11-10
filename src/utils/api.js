35552100c9e2946c20a431e7fb4a43dc61ea75a0
Mukesh <mukesh7rathore77@gmail.com>
2025-11-10 11:16:51 +0530

    Use axios wrapper + MockAPI via VITE_API_BASE and update tasks hook

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,13 @@
-import axios from "axios";          
+import axios from "axios";
+
+// Use environment variable if available, otherwise default to localhost for dev
+const BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000";
 
 const api = axios.create({
-    baseURL :"/api", 
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL: BASE,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
